f57c3c7a7e87e2f3c68d705ec9d5665ee68a5084
afrahzayan <afrahzayanzaya@gmail.com>
2026-05-16 15:36:14 +0530

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './Context/AuthContext.jsx'
+import { AuthProvider } from './context/authContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Wishlist from './Pages/Wishlist.jsx'
-import { WishlistProvider } from './Context/WishlistContext.jsx'
-import { CartProvider } from './Context/CartContext.jsx'
+import Wishlist from './pages/wishlist.jsx'
+import { WishlistProvider } from './context/wishlistContext.jsx'
+import { CartProvider } from './context/cartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
