e548d0ff1831dbd5fb2564a3ca92b69bd2f280c7
afrahzayan <afrahzayanzaya@gmail.com>
2026-05-16 16:21:15 +0530

    fix context

--- src/admin/component/AdminRoute.jsx
+++ src/admin/component/AdminRoute.jsx
@@ -1,6 +1,6 @@
 
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../../Context/AuthContext';
+import { useAuth } from '../../context/authContext';
 
 const AdminRoute = ({ children }) => {
   const { user, isInitializing } = useAuth();
--- src/admin/component/Dashboard.jsx
+++ src/admin/component/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../../Context/AuthContext';
+import { useAuth } from '../../context/authContext';
 
 const Dashboard = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
