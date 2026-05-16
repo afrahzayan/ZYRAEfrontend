d5f67bf559cb0bdccd985159032a5430e741ca7c
afrahzayan <afrahzayanzaya@gmail.com>
2026-05-16 16:10:30 +0530

    fix component

--- src/admin/pages/AddProduct.jsx
+++ src/admin/pages/AddProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api } from '../../api/axios';
-import Dashboard from '../Component/Dashboard';
+import Dashboard from '../component/Dashboard';
 
 const AddProduct = () => {
   const navigate = useNavigate();
